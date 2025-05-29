144c6b783b89086c2b4edc24347d3104111df86d
Mauricio Jaramillo <mauriciojaramillog@gmail.com>
2025-05-28 21:33:55 -0500

    Actualice archivo .env en frontend

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -4,6 +4,7 @@ import '../styles.css';
 import { Link } from 'react-router-dom';
 import { toast } from 'react-toastify';
 
+const API_URL = process.env.REACT_APP_API_URL;
 
 export default function Login() {
   const [email, setEmail] = useState('');
@@ -22,7 +23,7 @@ export default function Login() {
     setCargando(true);
 
     try {
-      const res = await fetch('http://localhost:5001/api/login', {
+      const res = await fetch(`${API_URL}/api/login`, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ email, password }),
